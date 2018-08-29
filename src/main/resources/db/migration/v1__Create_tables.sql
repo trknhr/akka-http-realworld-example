e2ac76761708c3c7fb928702c8181e6c50ffa791
Teruo Kunihiro <yuyuyu1999@gmail.com>
2018-08-29 18:52:40 +0900

    Start articles

--- src/main/resources/db/migration/v1__Create_tables.sql
+++ src/main/resources/db/migration/v1__Create_tables.sql
@@ -22,3 +22,16 @@ CREATE TABLE followers (
   FOREIGN KEY (user_id) REFERENCES users(id),
   CONSTRAINT follower_follower_follwed_unq UNIQUE (user_id, followee_id)
 )
+
+CREATE TABLE articles (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  slug VARCHAR(255) NOT NULL,
+  title VARCHAR(300) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL,
+  author_id INTEGER NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES users(id),
+  CONSTRAINT articles_slug_unique UNIQUE(slug)
+);
---
+++ src/main/scala/realworld/com/articles/ArticleRoutes.scala
@@ -0,0 +1,5 @@
+package realworld.com.articles
+
+class ArticleRoutes {
+
+}
