cee95bb9bc705bcc6be79b8d13b1b8406d94d111
Teruo Kunihiro <yuyuyu1999@gmail.com>
2019-08-04 21:36:38 +0900

    Add run-api-test on CI

---
+++ src/test/api_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export WORKING_DIR='api_test'
+echo "> Working dir: $WORKING_DIR"
+mkdir $WORKING_DIR
+
+echo "> Getting data..."
+git clone https://github.com/gothinkster/realworld.git $WORKING_DIR
+
+echo "> Start DB"
+docker-compose up -d
+
+echo "> Start API"
+sbt run start &
+
+cd ${WORKING_DIR}/realworld/api
+
+APIURL=localhost:9000/api ./$WORKING_DIR/realworld/api/run-api-tests.sh
+
+# Clean after test
+docker-compose down
+
