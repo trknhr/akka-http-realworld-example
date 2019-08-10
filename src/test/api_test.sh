d0c89b62d5bb08cea10767f92134f35d4040a593
Teruo Kunihiro <yuyuyu1999@gmail.com>
2019-08-10 11:27:12 +0900

    Update

--- src/test/api_test.sh
+++ src/test/api_test.sh
@@ -10,10 +10,8 @@ git clone https://github.com/gothinkster/realworld.git ./$WORKING_DIR
 echo "> Start Application "
 docker-compose up -d
 
-sleep 5
-
 echo "> Start Test"
-APIURL=localhost:9000/api ./$WORKING_DIR/api/run-api-tests.sh
+sleep 10;APIURL=localhost:9000/api ./$WORKING_DIR/api/run-api-tests.sh
 
 # Clean after test
 docker-compose down
